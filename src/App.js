f5c160c573b5b35c159611f8924a5493d48e2ecd
SUPITCHA JAMPATHONG <supitcha.gmail@mail.kmutt.ac.th>
2023-01-04 15:09:03 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Joonz
+          Muvi Web Application
         </a>
       </header>
     </div>
