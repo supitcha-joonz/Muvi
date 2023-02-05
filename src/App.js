a0a3a84ee690c1380907263984372ec4ee6feb3d
LAPTOP-MD2Q8PEA\Asus <supitcha.088@gmail.com>
2023-02-05 19:00:21 +0700

    add searchresult

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './containers/Administator/Home/Home';
 import Searchmain from './containers/SearchMain/Searchmain';
 import Login from './containers/Administator/Login/Login';
+import SearchResult from './containers/SearchResult/SearchResult';
 
 
 
@@ -21,6 +22,7 @@ function App() {
           
             <Route exact path="/" element={<Home />} />
             <Route exact path="/searchmain" element={<Searchmain />} />
+            <Route exact path="/searchresult" element={<SearchResult />} />
             <Route exact path="/login" element={<Login />} />
         
         </Routes>
